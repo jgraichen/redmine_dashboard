a34ee0f71dc46f5eaba6e927a69fc6aaef984de1
Jan Graichen <jan.graichen@altimos.de>
2011-05-17 09:08:13 +0000

    Update issue update action.

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -4,22 +4,34 @@ class DashboardController < ApplicationController
   before_filter :find_project
   
   def index
-    # issues ordered by priority desc
-    @issues = @project.issues.sort { |a,b| b.priority.position <=> a.priority.position }
-    @trackers = Tracker.find(:all)
-    @statuses = IssueStatus.find(:all)[0..2]
+    load_issues
   end
   
   def update_issue
-    # issues ordered by priority desc
-    @issues = @project.issues.sort { |a,b| b.priority.position <=> a.priority.position }
-    @trackers = Tracker.find(:all)
-    @statuses = IssueStatus.find(:all)[0..2]
+    issue = Issue.find(params[:issue]);
+    status = IssueStatus.find_by_position(params[:status]);
+    if !issue.nil? and !status.nil?
+      issue.status = status
+      issue.save
+    end
+    
+    @message = "Issue #{issue.id} changed to #{status.name}, maybe."
+    
+    load_issues
     
     render '_dashboard', :layout => false
+  rescue
+    render_500
   end
   
   private
+  def load_issues
+    # issues ordered by priority desc
+    @issues = @project.issues.sort { |a,b| b.priority.position <=> a.priority.position }
+    @trackers = Tracker.find(:all)
+    @statuses = IssueStatus.find(:all)
+  end
+  
   def find_project
     @project = Project.find(params[:id])
   end
