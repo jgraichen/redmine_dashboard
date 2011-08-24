5b410a6802a0e9b0d5aafa06b37e64708c625f9e
Jan Graichen <jan.graichen@altimos.de>
2011-08-24 19:02:59 +0200

    Remove old javascript file.

--- assets/javascripts/move.js
+++
@@ -1,8 +0,0 @@
-function moveItem(drag, drop){
-	drag.parentNode.removeChild(drag);
-	drag.setStyle({top:0,left:0});
-	drop.appendChild(drag);
-}
-function hideItem(drag, mouseevent) {
-	drag.element.parentNode.removeChild(drag.element);
-}
