b18b0d2f5993cda98887602d95d200dc371e5bf4
Jan Graichen <jg@altimos.de>
2012-11-17 18:17:06 +0100

    New routes for task and plannign board.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 # Plugin's routes
 # See: http://guides.rubyonrails.org/routing.html
 
-match 'projects/:id/dashboard', :controller => 'dashboard', :action => 'taskboard', :as => :taskboard
-match 'projects/:id/dashboard/planning', :controller => 'dashboard', :action => 'planboard', :as => :planboard
-match 'projects/:id/dashboard/configure', :controller => 'dashboard', :action => 'configure', :as => :configure_dashboard
+match 'projects/:id/rdb', :controller => 'rdb_dashboard', :action => 'index', :as => :rdb_dashboard
+match 'projects/:id/dashboard', :controller => 'rdb_dashboard', :action => 'index'
+match 'projects/:id/rdb/configure', :controller => 'rdb_dashboard', :action => 'configure', :as => :rdb_configure
+match 'projects/:id/rdb/taskboard', :controller => 'rdb_taskboard', :action => 'index', :as => :rdb_taskboard
+match 'projects/:id/rdb/planningboard', :controller => 'rdb_planningboard', :action => 'index', :as => :rdb_planningboard
