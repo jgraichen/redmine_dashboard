61b6fdf24cdb94e5dd99a879654a0f532f1bdc1c
Jan Graichen <jg@altimos.de>
2012-11-18 20:37:49 +0100

    Move columns to taskboard.

--- app/models/rdb_taskboard.rb
+++ app/models/rdb_taskboard.rb
@@ -2,12 +2,13 @@ class RdbTaskboard < RdbDashboard
 
   def init
     # Init filters
-    self << RdbDashboard::VersionFilter.new
-    self << RdbDashboard::TrackerFilter.new
-    self << RdbDashboard::AssigneeFilter.new
+    self.add_filter RdbDashboard::AssigneeFilter.new
+    self.add_filter RdbDashboard::VersionFilter.new
+    self.add_filter RdbDashboard::TrackerFilter.new
+    self.add_filter RdbDashboard::CategoryFilter.new
   end
 
-  def update(params)
+  def setup(params)
     super
 
     if ['tracker', 'priority', 'assignee', 'category', 'version', 'none'].include? params[:group]
@@ -25,45 +26,53 @@ class RdbTaskboard < RdbDashboard
 
   def build
     # Init columns
-    IssueStatus.sorted.each do |status|
-      next if status.is_closed?
-      self << RdbDashboard::Column.new(status.id, status.name,
-        :scope => Proc.new {|scope| scope.where(:status_id => status.id)},
-        :value => status)
+    done_statuses = IssueStatus.sorted.select do |status|
+      next true if status.is_closed?
+      self.add_column RdbTaskboard::Column.new("s#{status.id}", status.name, status)
+      false
     end
-    ids = IssueStatus.where(:is_closed => true).pluck(:id)
-    self << RdbDashboard::Column.new("done", :rdb_column_done,
-        :scope => Proc.new {|scope| scope.where(:status_id => ids)},
-        :hide => options[:hide_done])
+    self.add_column RdbTaskboard::Column.new("sX", :rdb_column_done, done_statuses, :hide => options[:hide_done])
 
     # Init groups
-    if compact?
-      case options[:group]
-      when :tracker
-        project.trackers.each do |tracker|
-          self << RdbDashboard::Group.new("tracker-#{tracker.id}", tracker.name, :accept => Proc.new {|issue| issue.tracker == tracker })
-        end
-      when :priority
-        IssuePriority.find(:all).reverse.each do |p|
-          self << RdbDashboard::Group.new("priority-#{p.position}", p.name, :accept => Proc.new {|issue| issue.priority_id == p.id })
-        end
-      when :assignee
-        self << RdbDashboard::Group.new(:assigne_me, :rdb_filter_assignee_me, :accept => Proc.new {|issue| issue.assigned_to_id == User.current.id })
-        self << RdbDashboard::Group.new(:assigne_none, :rdb_filter_assignee_none, :accept => Proc.new {|issue| issue.assigned_to_id.nil? })
-        self << RdbDashboard::Group.new(:assigne_other, :rdb_filter_assignee_others, :accept => Proc.new {|issue| !issue.assigned_to_id.nil? and issue.assigned_to_id != User.current.id })
-      when :category
-        project.issue_categories.each do |category|
-          self << RdbDashboard::Group.new("category-#{category.id}", category.name, :accept => Proc.new {|issue| issue.category_id == category.id })
-        end
-        self << RdbDashboard::Group.new(:category_none, :rdb_unassigned, :accept => Proc.new {|issue| issue.category.nil? })
-      when :version
-        project.versions.each do |version|
-          self << RdbDashboard::Group.new("version-#{version.id}", version.name, :accept => Proc.new {|issue| issue.fixed_version_id == version.id })
-        end
-        self << RdbDashboard::Group.new(:version_none, :rdb_unassigned, :accept => Proc.new {|issue| issue.fixed_version.nil? })
+    case options[:group]
+    when :tracker
+      project.trackers.each do |tracker|
+        self.add_group RdbDashboard::Group.new("tracker-#{tracker.id}", tracker.name, :accept => Proc.new {|issue| issue.tracker == tracker })
       end
+    when :priority
+      IssuePriority.find(:all).reverse.each do |p|
+        self.add_group RdbDashboard::Group.new("priority-#{p.position}", p.name, :accept => Proc.new {|issue| issue.priority_id == p.id })
+      end
+    when :assignee
+      self.add_group RdbDashboard::Group.new(:assigne_me, :rdb_filter_assignee_me, :accept => Proc.new {|issue| issue.assigned_to_id == User.current.id })
+      self.add_group RdbDashboard::Group.new(:assigne_none, :rdb_filter_assignee_none, :accept => Proc.new {|issue| issue.assigned_to_id.nil? })
+      self.add_group RdbDashboard::Group.new(:assigne_other, :rdb_filter_assignee_others, :accept => Proc.new {|issue| !issue.assigned_to_id.nil? and issue.assigned_to_id != User.current.id })
+    when :category
+      project.issue_categories.each do |category|
+        self.add_group RdbDashboard::Group.new("category-#{category.id}", category.name, :accept => Proc.new {|issue| issue.category_id == category.id })
+      end
+      self.add_group RdbDashboard::Group.new(:category_none, :rdb_unassigned, :accept => Proc.new {|issue| issue.category.nil? })
+    when :version
+      project.versions.each do |version|
+        self.add_group RdbDashboard::Group.new("version-#{version.id}", version.name, :accept => Proc.new {|issue| issue.fixed_version_id == version.id })
+      end
+      self.add_group RdbDashboard::Group.new(:version_none, :rdb_unassigned, :accept => Proc.new {|issue| issue.fixed_version.nil? })
     end
 
-    self << RdbDashboard::Group.new(:all, :rdb_all_issues) if groups.empty?
+    self.add_group RdbDashboard::Group.new(:all, :rdb_all_issues) if groups.empty?
+  end
+
+
+  # -------------------------------------------------------
+  # Helpers
+
+  def issues_for(column)
+    filter column.scope(project.issues)
+  end
+
+  def columns; @columns ||= HashWithIndifferentAccess.new end
+  def add_column(column)
+    column.board = self
+    columns[column.id.to_s] = column
   end
 end
---
+++ app/models/rdb_taskboard/column.rb
@@ -0,0 +1,32 @@
+class RdbTaskboard::Column
+  attr_accessor :board
+  attr_reader :name, :options, :id, :board, :statuses
+
+  def initialize(id, name, statuses, options = {})
+    @id       = id.to_s
+    @name     = name
+    @statuses = statuses.is_a?(Array) ? statuses : [statuses]
+    @options  = options
+  end
+
+  def scope(issue_scope)
+    issue_scope.where :status_id => statuses.map(&:id)
+  end
+
+  def issues
+    @issues ||= board.issues_for(self).select {|i| i.children.empty?}
+  end
+
+  def percentage
+    all_issue_count = board.issues.select {|i| i.children.empty?}.count
+    all_issue_count > 0 ? ((issues.count.to_f / all_issue_count) * 100).round(4) : 0
+  end
+
+  def title
+    name.is_a?(Symbol) ? I18n.translate(name) : name.to_s.humanize
+  end
+
+  def hide?
+    options[:hide]
+  end
+end
