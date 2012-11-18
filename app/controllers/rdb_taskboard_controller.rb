9aaf954b24c8a66c0e23f93e5982682743b05ae8
Jan Graichen <jg@altimos.de>
2012-11-18 21:27:41 +0100

    Improve move and update actions.

--- app/controllers/rdb_taskboard_controller.rb
+++ app/controllers/rdb_taskboard_controller.rb
@@ -22,21 +22,27 @@ class RdbTaskboardController < RdbDashboardController
     # Show dialog if more than one status are available
     return render 'rdb_dashboard/taskboard/column_dialog' if @statuses.count > 1
 
-    @issue.init_journal(User.current, params[:notes] || nil)
-    @issue.status         = @statuses.first
-    @issue.assigned_to_id = User.current.id if @board.options[:change_assignee]
-
-    @issue.save
-
-    render 'index'
+    params[:status] = @statuses.first.id
+    update
   end
 
   def update
     @issue.init_journal(User.current, params[:notes] || nil)
-    @issue.done_ratio = params[:done_ratio].to_i if params[:done_ratio]
 
+    @issue.done_ratio = params[:done_ratio].to_i if params[:done_ratio]
     @issue.assigned_to_id = nil if params[:unassigne_me] && @issue.assigned_to_id == User.current.id
     @issue.assigned_to_id = User.current.id if params[:assigne_me]
+
+    if params[:status]
+      status = IssueStatus.find params[:status].to_i
+      if @issue.new_statuses_allowed_to(User.current).include?(status) or User.current.admin?
+        @issue.status         = status
+        @issue.assigned_to_id = User.current.id if @board.options[:change_assignee]
+      else
+        return flash_error :rdb_flash_illegal_workflow_action,
+          :issue => @issue.subject, :source => @issue.status.name, :target => @status.name
+      end
+    end
 
     @issue.save
 
