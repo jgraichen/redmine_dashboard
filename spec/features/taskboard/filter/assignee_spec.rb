5111fc701585eb9fa0d83981bded60e84347af35
Jan Graichen <jg@altimos.de>
2013-07-17 10:35:05 +0200

    Assert specific number of matches.

--- spec/features/taskboard/filter/assignee_spec.rb
+++ spec/features/taskboard/filter/assignee_spec.rb
@@ -40,10 +40,8 @@ describe "Taskboard/Filter/Assignee", :js => true do
     unset_all_filter
     select_filter :assignee, 'Dave Lopper'
 
-    sleep 1
-
     page.should have_no_selector(:xpath, '//*[contains(@class, "rdb-property-assignee")][text()!="Dave Lopper"]')
-    page.should have_selector(:xpath, '//*[contains(@class, "rdb-property-assignee")][text()="Dave Lopper"]')
+    page.should have_selector(:xpath, '//*[contains(@class, "rdb-property-assignee")][text()="Dave Lopper"]', :count => 2)
   end
 
   it "should default to my issues" do
