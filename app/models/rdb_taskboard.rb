aef2941dae5f8c8ffb6cc2ff45ddec2c41c1b755
Jan Graichen <jg@altimos.de>
2013-12-20 21:04:24 +0100

    WIP

--- app/models/rdb_taskboard.rb
+++
@@ -1,3 +0,0 @@
-class RdbTaskboard < RdbBoard
-
-end
--- db/migrate/0002_add_board_type.rb
+++
@@ -1,9 +0,0 @@
-class AddProject < ActiveRecord::Migration
-  def self.up
-    add_column :rdb_boards, :project_id, :integer, :null => true
-  end
-
-  def self.down
-    remove_column :rdb_boards, :project_id
-  end
-end
