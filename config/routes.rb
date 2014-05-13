1a26a39099a4d4e735b6ffefb5b295bc40a5c717
Jan Graichen <jg@altimos.de>
2014-05-13 12:56:18 +0200

    Code style

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,10 @@
 get 'projects/:id/dashboards' => 'rdb_project_boards#index'
 get 'my/dashboards'           => 'rdb_user_boards#index'
 
-get  '/dashboards/:board_id'           => 'rdb#show',      :as => :rdb
-get  '/dashboards/:board_id/configure' => 'rdb#configure', :as => :configure_rdb
-post '/dashboards/:board_id'           => 'rdb#create'
-put  '/dashboards/:board_id'           => 'rdb#update'
+get '/dashboards/:board_id'           => 'rdb#show',      as: :rdb
+get '/dashboards/:board_id/configure' => 'rdb#configure', as: :configure_rdb
+post '/dashboards/:board_id'          => 'rdb#create'
+put '/dashboards/:board_id'           => 'rdb#update'
 
 if Rails.env.development?
   require 'sprockets'
@@ -17,9 +17,12 @@ if Rails.env.development?
   require 'stylus/sprockets'
   Stylus.setup environment
 
-  %w(app/assets/images app/assets/stylesheets app/assets/javascripts vendor/assets/javascripts).each do |source|
+  %w(app/assets/images
+     app/assets/stylesheets
+     app/assets/javascripts
+     vendor/assets/javascripts).each do |source|
     environment.append_path File.join('plugins', 'redmine_dashboard', source)
   end
 
-  mount environment, :at => '/rdb/assets'
+  mount environment, at: '/rdb/assets'
 end
