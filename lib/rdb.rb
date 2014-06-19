23ab5c4ae47ad15ea05b6a20d5f843b871f2c3ea
Jan Graichen <jg@altimos.de>
2014-06-19 14:57:21 +0200

    Add Sprocket::VERSION check to patch

--- lib/rdb.rb
+++ lib/rdb.rb
@@ -14,17 +14,19 @@ end
 
 require 'sprockets'
 
-# Required for MRI >= 2.0
-# https://github.com/sstephenson/sprockets/commit/cde6bc794a6b418da6009a2a2d05e44c2372eddf
-class Sprockets::DirectiveProcessor
-  def directives
-    @directives ||= header.lines.each_with_index.map { |line, index|
-      if directive = line[DIRECTIVE_PATTERN, 1]
-        name, *args = Shellwords.shellwords(directive)
-        if respond_to?("process_#{name}_directive", true)
-          [index + 1, name, *args]
+if Sprockets::VERSION < '2.2.2'
+  # Required for MRI >= 2.0
+  # https://github.com/sstephenson/sprockets/commit/cde6bc794a6b418da6009a2a2d05e44c2372eddf
+  class Sprockets::DirectiveProcessor
+    def directives
+      @directives ||= header.lines.each_with_index.map { |line, index|
+        if directive = line[DIRECTIVE_PATTERN, 1]
+          name, *args = Shellwords.shellwords(directive)
+          if respond_to?("process_#{name}_directive", true)
+            [index + 1, name, *args]
+          end
         end
-      end
-    }.compact#.tap{|d| p d }
+      }.compact#.tap{|d| p d }
+    end
   end
 end
