427526340a67b85598728960c8f1027a44bc2c04
Jan Graichen <jgraichen@altimos.de>
2021-11-21 17:22:12 +0100

    Revert "exp: Controller to generate SVG avatars"
    This reverts commit 2942ed8f13c417c06a2b178f7e3603ecd3f72078.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 # frozen_string_literal: true
 
-scope '/rdb', module: :rdb do
-  get 'avatar/:id', to: 'avatar#show', as: :rdb_avatar, format: :svg
-end
-
 get 'projects/:id/rdb', to: 'rdb_dashboards#index', as: :rdb_dashboards
 get 'projects/:id/rdb(/:board_id)', to: 'rdb_dashboards#show', as: :rdb_dashboard
 post 'projects/:id/rdb(/:board_id)', to: 'rdb_dashboards#update'
