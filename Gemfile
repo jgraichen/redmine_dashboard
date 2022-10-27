78942d9f7ead63e95cc05578c8a119a07f4e04ee
Jan Graichen <jgraichen@altimos.de>
2022-10-27 19:11:20 +0200

    chore: capybara uses webrick
    Remove old custom browser detection stuff. Seems to not be needed.

--- Gemfile
+++ Gemfile
@@ -7,8 +7,8 @@ send :ruby, RUBY_VERSION if ENV['CI']
 
 # Patch `#gem` to replace an already declared gem instead of flat our
 # refusing to do anything. This is important, for example, as our tests
-# require a specific version of `capybara` or `puma`, which is already
-# defined in Redmines Gemfile, but without any version constraint.
+# require a specific version of `capybara`, which is already defined in
+# Redmines Gemfile, but without the correct version constraint.
 def gem(name, *constraints)
   # Remove existing dependency
   @dependencies.reject! {|d| d.name == name }
@@ -26,7 +26,6 @@ end
 
 group :test do
   gem 'capybara', '~> 3.37.1'
-  gem 'puma', '< 6' # capybara < 3.38 (not yet released) breaks with Puma 6
   gem 'rubocop', '~> 1.37.0'
   gem 'slim_lint', '~> 0.22.1'
 end
--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -8,29 +8,6 @@ Capybara.javascript_driver = :selenium_chrome
 Capybara.default_max_wait_time = 15
 Capybara.server = :webrick
 
-# The test setup expects a linux system with `chromium` and
-# `chromium-chromedriver` installed. `chromedriver` must be available in PATH,
-# and is configured here to default to using `/usr/bin/chromium`. As both are
-# provided by the distribution their version should always match, even if
-# `google-chrome` is installed too.
-#
-if ENV['CHROME_PATH']
-  puts "Using chrome path from environment: #{ENV['CHROME_PATH']}"
-  Selenium::WebDriver::Chrome.path = ENV['CHROME_PATH']
-else
-  %w[
-    /usr/bin/chromium
-    /usr/bin/chromium-browser
-    /usr/bin/google-chrome
-  ].each do |path|
-    next unless File.executable?(path)
-
-    Selenium::WebDriver::Chrome.path = path
-    puts "Using detected chrome path: #{path}"
-    break
-  end
-end
-
 if %w[0 no off false].include?(ENV['HEADLESS'])
   Capybara.javascript_driver = :selenium_chrome
 else
