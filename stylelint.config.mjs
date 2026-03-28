bb9212ee52769702f22de0efb2977a5ce7b2e03d
Jan Graichen <jgraichen@altimos.de>
2026-03-28 13:55:08 +0100

    fix: stylelint checking for unsupported features

--- stylelint.config.mjs
+++ stylelint.config.mjs
@@ -1,10 +1,8 @@
 /** @type {import('stylelint').Config} */
 export default {
-  extends: [
-    "stylelint-config-standard",
-    "stylelint-no-unsupported-browser-features",
-  ],
+  extends: ["stylelint-config-standard"],
+  plugins: ["stylelint-no-unsupported-browser-features"],
   rules: {
-    "plugin/no-unsupported-browser-features": [true],
+    "plugin/no-unsupported-browser-features": [true, {}],
   },
 };
