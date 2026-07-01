656cd98dabe43e5503867e088ef8996574e768d2
Jan Graichen <jgraichen@altimos.de>
2026-07-01 21:53:13 +0200

    chore: Simplify and modernize helpers
    Replace hash-building with keyword arguments and modern Ruby syntax.
    This improves readability and maintainability of the code.
    
    Remove the custom `slim_tag` method and replace it with Rails' built-in
    `content_tag` and `tag` helpers, which are more idiomatic and easier to
    understand.

--- app/helpers/rdb_dashboard_helper.rb
+++ app/helpers/rdb_dashboard_helper.rb
@@ -1,106 +1,110 @@
 # frozen_string_literal: true
 
 module RdbDashboardHelper
-  def render_rdb_menu(id, title, options = {}, &container)
-    options[:class] ||= ''
-    options[:class] += ' rdb-menu-right' if options[:right]
-    options[:class] += ' rdb-small' if options[:small]
+  def rdb_menu(id, title, header: nil, **options, &container)
+    menu_classes = class_names(
+      options[:class],
+      'rdb-menu',
+      "rdb-menu-#{id}",
+      'rdb-menu-right': options[:right],
+      'rdb-small': options[:small],
+    )
 
-    slim_tag :div, class: "rdb-menu rdb-menu-#{id} #{options[:class]}" do
-      if options[:anchor]
-        link = options[:anchor].call.to_s.html_safe
-      else
-        link = link_to(title, '#', class: 'rdb-menu-link')
-      end
+    link = link_to(title, '#', class: 'rdb-menu-link')
 
-      if options[:header] && %i[h1 h2 h3 h4 h5].include?(options[:header].to_sym)
-        slim_tag(options[:header], link)
-      else
-        concat link
-      end
+    if %i[h1 h2 h3 h4 h5].include?(header)
+      link = content_tag(header, link)
+    end
 
-      slim_tag :div, class: "rdb-container #{'rdb-container-right' if options[:right]}" do
-        slim_tag :div, class: "rdb-container-wrapper #{'rdb-icons' if options[:icons]}" do
-          if options[:inlet]
-            slim_tag(:div, class: 'rdb-container-inlet', &container)
+    tag.div(class: menu_classes) do
+      concat link
+      concat(
+        tag.div(
+          class: class_names('rdb-container', 'rdb-container-right': options[:right]),
+        ) do
+          tag.div(
+            class: class_names('rdb-container-wrapper', 'rdb-icons': options[:icons]),
+          ) do
+            if options[:inlet]
+              tag.div(class: 'rdb-container-inlet', &container)
+            else
+              yield
+            end
+          end
+        end,
+      )
+    end
+  end
+
+  def rdb_menu_list( # rubocop:disable Metrics/ParameterLists
+    items = nil,
+    async: false,
+    title: nil,
+    list_tag: :ul,
+    list_class: nil,
+    **kwargs
+  )
+    list_classes = class_names(kwargs[:class], 'rdb-list', 'rdb-async': async)
+    tag.div(class: list_classes) do
+      concat tag.h3(title) if title.present?
+
+      concat(
+        content_tag(list_tag, class: list_class) do
+          if items
+            items.each do |item|
+              concat(tag.li { yield item })
+            end
           else
             yield
           end
-        end
-      end
+        end,
+      )
     end
   end
 
-  def render_rdb_menu_list(items = nil, options = {})
-    if items.is_a?(Hash)
-      options = items
-      items = nil
-    end
-
-    slim_tag :div, class: "rdb-list #{'rdb-async' if options[:async]} #{options[:class]}" do
-      slim_tag :h3, options[:title] if options[:title]
-      slim_tag options[:list_tag] || :ul, class: options[:list_class] do
-        if items
-          items.each do |item|
-            slim_tag :li do
-              yield item
-            end
-          end
-        else
-          yield
-        end
-      end
-    end
+  def rdb_menu_list_item(classes: nil, async: nil, &block)
+    tag.li(
+      class: class_names(classes, 'rdb-async': async),
+      &block
+    )
   end
 
-  def render_rdb_menu_list_item(options = {}, &block)
-    slim_tag(:li, class: options[:async] ? 'rdb-async' : '', &block)
+  def rdb_checkbox_link_to(*args, enabled: nil, show_disabled: nil, **kwargs)
+    kwargs[:class] = class_names(
+      kwargs[:class],
+      'rdb-checkbox-link',
+      'rdb-checkbox-link-enabled': enabled,
+      'rdb-checkbox-link-disabled': !enabled && show_disabled,
+    )
+
+    link_to(*args, **kwargs)
   end
 
-  def rdb_checkbox_link_to(*args)
-    options = args.extract_options!
-    options[:class] ||= ''
-    options[:class] += ' rdb-checkbox-link'
-    options[:class] += ' rdb-checkbox-link-enabled' if options[:enabled]
-    options[:class] += ' rdb-checkbox-link-disabled' if !options[:enabled] && options[:show_disabled]
-    options[:class].strip!
-
-    options.delete :enabled
-    options.delete :show_disabled
-
-    args << options
-
-    link_to(*args)
-  end
-
-  def rdb_update_path(issue, options = {})
-    send(:"rdb_#{@board.id}_update_path", {
+  # rubocop:disable Rails/HelperInstanceVariable
+  def rdb_update_path(issue, **kwargs)
+    send(
+      :"rdb_#{@board.id}_update_path",
       issue: issue.id,
-      lock_version: issue.lock_version
-    }.merge(options),)
+      lock_version: issue.lock_version,
+      **kwargs,
+    )
   end
 
-  def rdb_move_path(issue, options = {})
-    send(:"rdb_#{@board.id}_move_path", {
+  def rdb_move_path(issue, **kwargs)
+    send(
+      :"rdb_#{@board.id}_move_path",
       issue: issue.id,
-      lock_version: issue.lock_version
-    }.merge(options),)
+      lock_version: issue.lock_version,
+      **kwargs,
+    )
   end
 
-  def rdb_filter_path(options = {})
-    send(:"rdb_#{@board.id}_filter_path", options)
+  def rdb_filter_path(**kwargs)
+    send(:"rdb_#{@board.id}_filter_path", **kwargs)
   end
 
-  def rdb_board_path(options = {})
-    send(:"rdb_#{@board.id}_path", options)
+  def rdb_board_path(**kwargs)
+    send(:"rdb_#{@board.id}_path", **kwargs)
   end
-
-  private
-
-  def slim_tag(name, content = nil, **attrs)
-    concat tag(name, attrs, true, true)
-    concat(content) if content.present?
-    yield if block_given?
-    concat "</#{name}>".html_safe
-  end
+  # rubocop:enable Rails/HelperInstanceVariable
 end
